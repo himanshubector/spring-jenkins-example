f742c44181992422f9c42e355a576b78c2594655
himanshubector <smk.131@gmail.com>
2023-09-08 16:20:04 +0200

    added logger statement in test class

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -16,6 +16,7 @@ class SpringJenkinsApplicationTests
 	void contextLoads()
 	{
 		log.info("Test case executing............................");
+		log.info("Test case executing - Added another log statement in Test class............................");
 		assertEquals(true, true);
 	}
 
